d6f36584e86316e68186addea266146c70a9a2d3
Wasik Hasan <wasikhasan123@gmail.com>
2026-06-07 19:06:51 +0600

    deploy: Firestore payment submission + admin review workflow

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1291441773" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2491633651" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
