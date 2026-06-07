3e2062bdbd371a004e79491068d1cfebf1053110
Wasik Hasan <wasikhasan123@gmail.com>
2026-06-07 13:59:58 +0600

    deploy: update payment number to 01813059794

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1055879915" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2029484298" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
