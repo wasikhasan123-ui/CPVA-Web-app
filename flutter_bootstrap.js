7c90f76c2ae48e8cd9eff0b62832ec8883313039
Wasik Hasan <wasikhasan123@gmail.com>
2026-06-07 17:37:08 +0600

    deploy: ui - admin dashboard polish

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2285650546" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3950032313" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
